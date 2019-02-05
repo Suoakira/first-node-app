e79c79167ccb594f792df894bdbef3b213991354
github username <suoarik@gmail.com>
2019-02-05 16:33:38 +0000

    importing an using node modules

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const sayHello = (name) => {
-    return console.log("hello")
-}
+// importing modules
+const path = require("path")
+const os = require("os")
 
-setTimeout()
+// converts path to object, easier to work with than using strings
+let pathObject = path.parse(__filename)
+let totalMemmory = os.totalmem()
+let freeMemory = os.freemem()
+
+
+console.log(pathObject)
+
+console.log(totalMemmory)
+console.log(freeMemory)
--- logger.js
+++ logger.js
@@ -1,9 +1,15 @@
+
+// module wrapper functions
 let url = "httpL//mylogger.io/log"
+console.log(__filename)
+console.log(__dirname)
+
 
 const log = (message) => {
     // send http request
     console.log(message)
 }
 
-module.exports.log = log
-module.exports.url = url
+module.exports = log
+
+
