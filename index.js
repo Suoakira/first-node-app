6ffbb4a6e43fe02a2e9ac7d554aaeef54d28fc78
github username <suoarik@gmail.com>
2019-02-05 17:57:44 +0000

    added post requests with express

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require('express')
-
 const app = express()
+app.use(express.json())
 
+const courses = [
+    {id: 1, name: "course1"}
+]
 app.get('/', (req, res) => {
     res.send('Hello World, Test1')
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3])
+    res.send(courses)
 })
 
 // can use req.params.id to get requests
-app.get('/api/courses/:id/:year', (req, res) => {
-    res.send(req.params)
+app.get('/api/courses/:id', (req, res) => {
+    // res.send(req.params)
+    let course = courses.find(course => course.id === parseInt(req.params.id))
+    console.log(course)
+    if (!course) {
+        res.status(404).send("Course with given id was not found")
+    } else {
+        res.send(course)
+    }
+})
+
+app.post('/api/courses', (req, resp) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    resp.send(course)
+    console.log(courses)
 })
 
 // PORT enviroment variable
